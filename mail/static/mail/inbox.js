c5757627a2a2cbd072c8772ab509b2dc4ea1b52d
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-03-25 11:10:11 -0400

    Fin load_mailbox, load_email, send_mail funcs
    Add email-view div to inbox.html
    
    Fix comment typo views.py 'chronologial'

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit', send_mail);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -22,6 +23,7 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
@@ -30,4 +32,109 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Get the emails
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+
+    // Print emails
+    console.log(emails);
+
+    // Loop through the emails
+    emails.forEach(email => {
+      // Create a div for the email
+      const recipients = email.recipients.join(', ')
+      const recipients_concat = recipients.length > 25 ? recipients.substring(0, 25) + '...' : recipients;
+      const subject = email.subject ? email.subject : '(no subject)';
+      const email_div = document.createElement('div');
+      
+      email_div.className = 'email';
+      email_div.innerHTML = `<b>To: ${recipients_concat}</b> - ${subject} - ${email.timestamp}`;
+      email_div.addEventListener('click', () => {
+        load_email(email.id);
+      });
+
+      // Append the email to the emails-view
+      document.querySelector('#emails-view').appendChild(email_div);
+    });
+  });
+
+}
+
+
+function load_email(email_id) {
+  // Show the email-view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'block';
+
+  // Get the email
+  fetch(`/emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+    // Print email
+    console.log(email);
+
+    // Create a div for the email
+    const email_div = document.createElement('div');
+    email_div.innerHTML = `<b>From:</b> ${email.sender}<br><b>To:</b> ${email.recipients}<br><b>Subject:</b> ${email.subject}<br><b>Timestamp:</b> ${email.timestamp}<br><br>${email.body}`;
+
+    // Append the email to the email-view
+    document.querySelector('#email-view').appendChild(email_div);
+
+    // Create a button to reply
+    const reply_button = document.createElement('button');
+    reply_button.innerHTML = 'Reply';
+    reply_button.addEventListener('click', () => {
+      compose_email();
+      document.querySelector('#compose-recipients').value = email.sender;
+      document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
+      document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} wrote: ${email.body}`;
+    });
+
+    // Append the button to the email-view
+    document.querySelector('#email-view').appendChild(reply_button);
+
+    // Create a button to archive
+    const archive_button = document.createElement('button');
+    archive_button.innerHTML = 'Archive';
+    archive_button.addEventListener('click', () => {
+      archive_email(email_id, email.archived);
+    });
+
+    // Append the button to the email-view
+    document.querySelector('#email-view').appendChild(archive_button);
+
+    // Mark the email as read
+    fetch(`/emails/${email_id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        read: true
+      })
+    });
+  });
+}
+
+
+function send_mail() {
+  // Get the values of the email form
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  // Send the email
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    // Print result
+    console.log(result);
+  });
 }
